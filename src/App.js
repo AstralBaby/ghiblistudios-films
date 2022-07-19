281ff9a81df06be6c81b094b7af04c8f94f70301
Catalina Fernandez <catalina.fernandez@m6connect.com>
2022-07-19 15:52:07 -0300

    Add basic project structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/styles/tailwind.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-red-300">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
