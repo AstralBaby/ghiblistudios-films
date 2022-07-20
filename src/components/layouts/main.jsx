d8cd1b0ca667e7e4ef1fe98b3c6c6a61b406f3a9
Catalina Fernandez <catalina.fernandez@m6connect.com>
2022-07-20 19:56:37 -0300

    update layout

--- src/components/layouts/main.jsx
+++ src/components/layouts/main.jsx
@@ -2,11 +2,11 @@ import logo from '../../assets/logox600.png'
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="w-screen h-screen bg-white/60">
-            <div>
-                <img style={{width: 250}} src={logo} alt="Ghibli Studios" />
+        <div className="w-screen h-screen bg-white/60 flex flex-col">
+            <div className="py-5">
+                <img style={{width: 250}} src={logo} alt="Ghibli Studios" className="mx-auto" />
             </div>
-            <div className="p-10 container mx-auto">
+            <div className="p-10 flex-1">
                 {children}
             </div>
         </div>
