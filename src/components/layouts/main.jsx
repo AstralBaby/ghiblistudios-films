94cc4b6a2d4253cf5adf295fa9552ea72fa51ac5
Catalina Fernandez <catalina.fernandez@m6connect.com>
2022-07-22 16:28:34 -0300

    add scrolling for film details

--- src/components/layouts/main.jsx
+++ src/components/layouts/main.jsx
@@ -2,7 +2,7 @@ import logo from '../../assets/logox600.png'
 
 const MainLayout = ({ children }) => {
     return (
-        <div className="w-screen h-screen bg-white/60 flex flex-col">
+        <div className="w-screen h-screen bg-white/60 overflow-y-scroll flex flex-col">
             <div className="py-5">
                 <img style={{width: 250}} src={logo} alt="Ghibli Studios" className="mx-auto" />
             </div>
