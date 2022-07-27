36532d98fc1ad8a122ebd6cd78ef401f305c464f
Catalina Fernandez <catalina.fernandez@m6connect.com>
2022-07-27 17:51:50 -0300

    fixed carousel scroll

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,16 +6,18 @@ import SearchContext from "../contexts/SearchContext";
 import { viewportMatchingValue } from "../utils/responsive";
 
 const HomePage = () => {
-    const carouselInitialPosition = viewportMatchingValue(1, 6, 6, 6)
     const [films, setFilms] = useState([])
     const carousel = useRef(null)
-    const [carouselPosition, setCarouselPosition] = useState(carouselInitialPosition)
+    const [currentFilm, setCurrentFilm] = useState(0)
     const [noResults, setNoResults] = useState(false)
+    const [scroll, setScroll] = useState(0)
+    const [maxScroll, setMaxScroll] = useState(0)
     //todo add transition hook to add loader when there is no movies
 
     useEffect(() => {
         axios.get('https://ghibliapi.herokuapp.com/films').then(({data}) => setFilms(data))
-    }, [])
+        setMaxScroll(carousel.current.scrollWidth - carousel.current.clientWidth)
+    }, [films, maxScroll])
 
     const moveCarousel = (isNext) => {
         const colSize = carousel.current?.clientWidth / viewportMatchingValue(1, 6, 6, 6)
@@ -23,7 +25,6 @@ const HomePage = () => {
             left: isNext? colSize : -colSize,
             behavior: 'smooth'
         })
-        setCarouselPosition(prev => isNext ? prev + 1 : prev - 1)
     }
     const slideNext = () => moveCarousel(true)
     const slidePrev = () => moveCarousel()
@@ -68,7 +69,7 @@ const HomePage = () => {
             <SearchContext.Consumer>
                 {searchParams => (
                     <div className="h-full flex flex-col">
-                    <img src={films.length && films[carouselPosition].image} alt="" className="fixed blur-2xl w-screen h-screen object-fit" style={{zIndex: -1}} />
+                    <img src={films.length && films[currentFilm].image} alt="" className="fixed blur-2xl w-screen h-screen object-fit" style={{zIndex: -1}} />
                     {noResults && (
                         <div className="p-3 w-1/4 mx-auto rounded bg-gray-200 text-gray-700 font-medium text-sm">
                             <i className="bx bx-sad mr-1 text-lg align-middle" />
@@ -76,21 +77,23 @@ const HomePage = () => {
                         </div>
                     )}
                     <div className="flex items-center scroll-smooth">
-                        <button disabled={carouselPosition === carouselInitialPosition} onClick={slidePrev} className="flex-shrink-0 w-12 h-12 enabled:bg-gray-900 bg-gray-400 rounded-full">
+                        <button disabled={scroll === 0} onClick={slidePrev} className="flex-shrink-0 w-12 h-12 enabled:bg-gray-900 bg-gray-400 rounded-full">
                             <i className='bx bx-chevron-left text-4xl text-white'></i>
                         </button>
-                        <div ref={carousel} className="flex overflow-hidden mx-5 flex-grow">
+                        <div ref={carousel} onScroll={e => setScroll(e.target.scrollLeft)} className="flex overflow-hidden mx-5 flex-grow">
                             {filteredFilms(searchParams).map((entry, idx) => (
-                                <div key={idx} className="flex-none p-5 w-full md:w-1/6 lg:w-2/12 xl:w-2/12">
+                                <div onClick={() => setCurrentFilm(idx)} key={idx} className="cursor-pointer flex-none p-5 w-full md:w-1/6 lg:w-2/12 xl:w-2/12">
                                     <MovieCard title={entry.title} thumb={entry.image}></MovieCard>
                                 </div>
                             ))}
                         </div>
-                        <button disabled={carouselPosition === films.length} onClick={slideNext} className="enabled:bg-gray-900 flex-shrink-0 w-12 h-12 bg-gray-400 rounded-full">
+                        <button disabled={scroll === maxScroll} onClick={slideNext} className="enabled:bg-gray-900 flex-shrink-0 w-12 h-12 bg-gray-400 rounded-full">
                             <i className='bx bx-chevron-right text-4xl text-white'></i>
                         </button>
                     </div>
-                    {films.length && <FilmDetails film={films[carouselPosition]} />}
+                    {films.length && currentFilm && (
+                        <FilmDetails film={films[currentFilm]} />
+                    )}
                 </div>
                 )}
             </SearchContext.Consumer>
