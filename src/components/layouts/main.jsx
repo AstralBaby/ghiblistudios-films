2cbc09ce3eef62a323ed68acd9616cafaabb625f
Catalina Fernandez <catalina.fernandez@m6connect.com>
2022-07-28 12:51:27 -0300

    add outside click detection to searchbox and search clear btn

--- src/components/layouts/main.jsx
+++ src/components/layouts/main.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import logo from '../../assets/logox600.png'
 import SearchContext from '../../contexts/SearchContext'
 
@@ -10,15 +10,29 @@ const MainLayout = ({ children }) => {
     const [dateFrom, setDateFrom] = useState('')
     const [dateTo, setDateTo] = useState('')
     const [searchQuery, setSearchQuery] = useState('')
+    const isDateRangeActive = dateFrom && dateTo
+    const searchParams = useMemo(() => ({dateFrom, dateTo, searchQuery}), [dateFrom, dateTo, searchQuery])
 
-    const classes = {
-        searchBox: `shadow focus-within:shadow-lg border w-1/2 rounded-lg bg-white ${showFilter ? 'absolute pb-1' : 'relative'}`,
-        searchInput: `flex py-1 px-2 ${showFilter && 'border-b border-gray-200'}`,
-        filterBtn: 'bg-gray-300 rounded-lg text-gray-700 text-sm font-medium py-2 px-4',
-        datePicker: 'border border-gray-300 p-1 rounded ml-3'
+    const removeFilters = () => {
+        setDateFrom('')
+        setDateTo('')
+        setSearchQuery('')
     }
 
-    const searchParams = useMemo(() => ({dateFrom, dateTo, searchQuery}), [dateFrom, dateTo, searchQuery])
+    useEffect(() => {
+        window.addEventListener('click', event => {   
+            if (!document.getElementById('searchbox').contains(event.target)){
+              setShowFilter(false)
+            }
+        })
+    })
+
+    const classes = {
+        searchBox: `shadow focus-within:shadow-lg border w-3/4 rounded-lg bg-white ${showFilter ? 'absolute pb-1' : 'relative'}`,
+        searchInput: `flex py-1 px-2 ${showFilter && 'border-b border-gray-200'}`,
+        filterBtn: `rounded-lg text-sm font-medium py-2 px-4 border border-gray-300 ${isDateRangeActive ? 'bg-blue-600 text-white' : 'text-gray-700'}`,
+        datePicker: 'border border-gray-300 p-1 rounded ml-3'
+    }
 
     return (
         <div className="w-screen h-screen bg-white/60 overflow-y-scroll flex flex-col">
@@ -26,14 +40,19 @@ const MainLayout = ({ children }) => {
                 <div className="flex-1">
                     <img style={{width: 200}} src={logo} alt="Ghibli Studios" className="" />
                 </div>
-                <div className="flex-1 flex justify-center relative">
+                <div className="flex-1 flex justify-center relative" id="searchbox">
                     <div className={classes.searchBox}>
                         <div className={classes.searchInput}>
                             <input type="search" placeholder="Search films by author, director" className="flex-grow rounded-l-lg outline-none"
                                     onChange={e => setSearchQuery(e.target.value)} value={searchQuery} />
-                            <div className="ml-1 self-center">
-                                <button onClick={toggleFilter} className={classes.filterBtn}>Filter by</button>
+                            <div className="px-1 self-center">
+                                <button onClick={toggleFilter} className={classes.filterBtn}>
+                                    {isDateRangeActive ? 'Filtering by date' : 'Select date'}
+                                </button>
                             </div>
+                            <button className="p-1 cursor-pointer" onClick={removeFilters}>
+                                <i className="bx bx-x text-gray-400 text-xl align-middle" />
+                            </button>
                         </div>
                         {showFilter && (
                             <div className="bg-white w-full p-3 text-gray-700">
@@ -46,7 +65,7 @@ const MainLayout = ({ children }) => {
                                     <div>
                                         <input type="date" min="1984-03-11" onChange={e => setDateFrom(e.target.value)}
                                             value={dateFrom} className={`${classes.datePicker} ml-3`} />
-
+                                        <br />
                                         <input type="date" min={searchQuery.dateFrom} onChange={e => setDateTo(e.target.value)}
                                             value={dateTo} className={classes.datePicker} />
                                     </div>
